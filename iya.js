c5fadbbe98f55dbda67bb4ba2568e29af6d42826
rezaarap2323 <157443677+rezaarap2323@users.noreply.github.com>
2024-05-31 16:20:53 +0800

    LAB 1 JS

---
+++ iya.js
@@ -0,0 +1,14 @@
+alert("TUGAS LAB JS");
+
+
+function henshin(){
+    document.getElementById("contoh").innerHTML = "Horeee Berubah";
+
+}
+
+function nambah(){
+    document.getElementById("contoh1").innerHTML = "Horeee Nambah";
+}
+
+
+
